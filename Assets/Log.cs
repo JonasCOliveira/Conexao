a3c4ed1d66c0aabab8dd4cd316655403acebd634
JonasCOliveira <jonascauan@outlook.com>
2021-03-03 14:55:30 -0300

    v01

---
+++ Assets/Log.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Log : MonoBehaviour
+{
+
+    public GameHandler gh = new GameHandler();
+
+    private void Awake() {
+
+        gh = FindObjectOfType<GameHandler>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    public IEnumerator RegisterLog(string playerName, int level)
+    {
+   
+       
+        // Create a form object 
+        WWWForm form = new WWWForm();
+        form.AddField("Jogador", playerName );
+        form.AddField("Fase", level);
+        form.AddField("Fitness", gh.getFitness().ToString());
+        form.AddField("Score", gh.getScoreFunction().ToString());
+        form.AddField("DiamantesColetados", GameHandler.numberOfColectedCoins);
+        form.AddField("DiamantesVistos", GameHandler.numberOfTotalCoins);
+        form.AddField("VidasColetadas",GameHandler.numberOfBonusLife);
+        form.AddField("VidasRestantes",GameHandler.numberOfRemainingLife);
+        form.AddField("qtdAguia", 1);
+        form.AddField("qtdEspinho",11);
+        form.AddField("qtdGamba",21);
+        form.AddField("qtdSapo",31);
+
+        // Debug.Log("");
+
+
+        using (UnityWebRequest www = UnityWebRequest.Post("https://roozadventure.000webhostapp.com/",form)){
+             yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError){
+
+                Debug.Log( "Error: " + www.error );
+                Debug.Log(www.url);
+            } else {
+                Debug.Log("chegou a inserir");
+                Debug.Log(www.downloadHandler.text);
+            }
+
+        }
+
+    }
+}
---
+++ Assets/Play.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+    public Button playButton;
+    public InputField usernameInput;
+
+    public Text nameAlert;
+
+     private void Start() {
+        
+		Button btn = playButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+        nameAlert.GetComponent<Text>().enabled = false;
+     }
+
+    void TaskOnClick(){
+
+        if(usernameInput.text != ""){
+            PlayerPrefs.SetString("playersName", usernameInput.text);
+            PlayerPrefs.SetInt("level", SceneManager.GetActiveScene().buildIndex);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            
+        }
+        else {
+            nameAlert.GetComponent<Text>().enabled = true;
+        }
+	}
+
+
+}
